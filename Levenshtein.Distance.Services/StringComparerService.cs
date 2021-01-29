4409d530d006def79c30fc25d4bbb4808fad200e
Aniket Joshi <59261142+aniketljoshi@users.noreply.github.com>
2021-01-29 23:31:15 +0530

    Added string compaer service

---
+++ Levenshtein.Distance.Services/StringComparerService.cs
@@ -0,0 +1,27 @@
+﻿using Levenshtein.Distance.Core;
+using System.Threading.Tasks;
+
+namespace Levenshtein.Distance.Services
+{
+    public class StringComparerService : IStringComparerService
+    {
+        private readonly IComparerProvider _comparerProvider;
+
+        public StringComparerService(IComparerProvider comparerProvider)
+        {
+            _comparerProvider = comparerProvider;
+        }
+
+        public async Task<StringCompareResponse> CompareAsync(StringCompareRequest request)
+        {
+            request.IsValid();
+
+            var response = await _comparerProvider.CompareAsync(request.FirstString, request.SecondString);
+
+            return new StringCompareResponse()
+            {
+                OutPut = response
+            };
+        }
+    }
+}
