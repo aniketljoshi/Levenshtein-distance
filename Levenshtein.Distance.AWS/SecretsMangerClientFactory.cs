00ef9f11103fcfec25406434a44fb907f517ac89
Aniket Joshi <59261142+aniketljoshi@users.noreply.github.com>
2021-01-29 23:30:37 +0530

    Added client for SM

---
+++ Levenshtein.Distance.AWS/SecretsMangerClientFactory.cs
@@ -0,0 +1,30 @@
+﻿using Amazon.SecretsManager;
+using Levenshtein.Distance.Core;
+using System.Threading.Tasks;
+
+namespace Levenshtein.Distance.AWS
+{
+    public class SecretsMangerClientFactory : ISecretsMangerClientFactory
+    {
+        private static IAmazonSecretsManager _client;
+
+        public async Task<IAmazonSecretsManager> GetClientAsync(SecretsManagerConfiguration configuration)
+        {
+            if (_client != null)
+                return _client;
+            else
+            {
+                if (_client != null)
+                    return _client;
+
+                _client = CreateClient(configuration);
+                return _client;
+            }
+        }
+
+        private AmazonSecretsManagerClient CreateClient(SecretsManagerConfiguration configuration)
+        {
+            return new AmazonSecretsManagerClient(Utility.GetRegionEndpoint(configuration.Region));
+        }
+    }
+}
